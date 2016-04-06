cda131b2d55da8b61992349eb03aa458f97e136e
HuangGuSheng <huanggusheng520@sina.com>
2016-04-06 20:14:28 +0800

    从头开始，毕业设计
    一个可以发帖求助的app

---
+++ app/src/main/java/com/huanggusheng/graduationproject/model/Post.java
@@ -0,0 +1,57 @@
+package com.huanggusheng.graduationproject.model;
+
+/**
+ * Created by Huang on 2016/4/6.
+ */
+
+import com.avos.avoscloud.AVClassName;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVRelation;
+
+import java.util.List;
+
+@AVClassName("Post")
+public class Post extends AVObject{
+    public static final Creator CREATOR = AVObjectCreator.instance;
+
+    public static final String CONTENT = "content";
+    public static final String AUTHOR = "author";
+    public static final String LIKES = "likes";
+    public static final String REWARDS = "rewards"; // 打赏
+
+    public Post() {
+
+    }
+
+    public User getAuthor() {
+        return getAVObject(AUTHOR);
+    }
+
+    public void setAuthor(User author) {
+        put(AUTHOR, author);
+    }
+
+    public String getContent() {
+        return getString(CONTENT);
+    }
+
+    public void setContent(String content) {
+        put(CONTENT, content);
+    }
+
+    public List<User> getLikes() {
+        return getList(LIKES);
+    }
+
+    public void setLikes(List<User> likes) {
+        put(LIKES, likes);
+    }
+
+    public AVRelation<User> getRewardStudents() {
+        return getRelation(REWARDS);
+    }
+
+    public void setRewardStudents(AVRelation<User> rewardStudents) {
+        put(REWARDS, rewardStudents);
+    }
+}
---
+++ app/src/main/java/com/huanggusheng/graduationproject/model/User.java
@@ -0,0 +1,10 @@
+package com.huanggusheng.graduationproject.model;
+
+import com.avos.avoscloud.AVObject;
+
+/**
+ * Created by Huang on 2016/4/6.
+ */
+public class User extends AVObject {
+
+}
